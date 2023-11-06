1c585e2f8943accd29fa479716a4972547ed6121
Kavya Karunakaran <90122782+kavya-vk@users.noreply.github.com>
2023-11-06 13:21:43 -0800

    distance methods implemented

--- DistanceCalculator.py
+++ DistanceCalculator.py
@@ -1,1 +1,32 @@
-class 
+import pandas as pd
+import numpy as np
+
+class DistanceCalculator:
+    """
+    x and y are rows of a pandas dataframe, each representing a datapoint
+    """
+    def euclidian_distance(x, y):
+        # dist = sqrt(sum((x-y)^2))
+        diff = x - y
+        return np.sqrt(np.sum(diff ** 2))
+    
+    def squared_euclidian_distance(x, y):
+        # dist = sum((x-y)^2))
+        diff = x - y
+        return np.sum(diff ** 2)
+    
+    def manhattan_distance(x, y):
+        diff = x - y
+        return np.sum(abs(diff))
+    
+    def minkowski_distance(x, y, l):
+        """
+        l = lambda 
+        l = 1 manhattan distance, l = 2 euclidian distance
+        """
+        diff = x - y
+        return np.power(np.sum((diff ** l)), 1/l)
+    
+    def chebyshev_distance(x, y):
+        diff = x - y
+        return np.max(abs(diff))
