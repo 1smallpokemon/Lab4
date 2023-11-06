c81e1e0e02bb7613f296eca756499d559153a5d8
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-06 12:46:25 -0800

    updated preproccessing for clustering

--- preprocessing.py
+++ preprocessing.py
@@ -1,48 +1,36 @@
 import pandas as pd
 import numpy as np
-from pandas.api.types import is_any_real_numeric_dtype
 
+def load_data(file_path):
+    """Loads data from a CSV file, excluding columns marked with 0 in the first row."""
+    data = pd.read_csv(file_path, header=None)
+    # Get the binary vector from the first row
+    use_columns = data.iloc[0, :].astype(bool).tolist()
+    # Exclude columns based on the binary vector
+    data = data.loc[:, use_columns]
+    # Remove the first row as it is just the binary vector
+    data = data.iloc[1:, :]
+    return data.reset_index(drop=True)
 
-def get_metadata(D: pd.core.frame.DataFrame)->tuple[str, np.ndarray]:
-    """Returns the class name and the vector of domain size for each column except the class column"""
-    class_name = D.iloc[1,0]
-    domain_size_vector = D.iloc[0,:]
-    if domain_size_vector.hasnans: #this is just for the mushroom dataset, which leaves out the domain size of the class column!
-        domain_size_vector = domain_size_vector.dropna()
-    else:
-        without_class = D.loc[:,D.columns != class_name]
-        domain_size_vector = without_class.iloc[0,:]
-    if len(domain_size_vector) != D.shape[1] - 1:
-        raise pd.errors.ParserError("Metadata unreadable")
-    return class_name, np.array(domain_size_vector, dtype=int)
+def preprocess_data(data):
+    """Preprocesses the data for clustering."""
+    # Convert all columns to numeric
+    for col in data.columns:
+        data[col] = pd.to_numeric(data[col], errors='coerce')
+    # Replace '?' with NaN and drop rows with NaN values
+    data = data.replace('?', np.nan).dropna()
+    # Normalize the data
+    return normalize(data)
 
-def get_data(D: pd.core.frame.DataFrame)->tuple[pd.core.frame.DataFrame, pd.core.series.Series]:
-    """Returns a dataset X and a list of class labels y"""
-    class_name, domain_size_vector = get_metadata(D)
-    without_metadata = D.drop(index=[0,1])
-    X = without_metadata.loc[:,without_metadata.columns != class_name]
-    y = without_metadata[class_name].astype("str")
-    #assert(len(X.columns) == len(domain_size_vector))
-    for col_name, domain_size in zip(X.columns, domain_size_vector):
-        if domain_size == 0:
-            X.loc[:,col_name] = pd.to_numeric(X[col_name], errors='coerce')
-        else:
-            X.loc[:,col_name] = X[col_name].astype("str")
-    X = X.replace('?', np.nan) #other values to be interpreted as null?
-    X = X.reset_index(drop=True)
-    y = y.reset_index(drop=True)
-    return X, y
+def normalize(data):
+    """Normalizes the numeric columns in the data."""
+    for col in data.columns:
+        if is_any_real_numeric_dtype(data[col]):
+            data[col] = normalize_column(data[col].values)
+    return data
 
-def normalize_column(X):
-    mins = np.min(X, axis=0)
-    maxs = np.max(X, axis=0)
-    return np.nan_to_num((X - mins) / (maxs - mins))
-
-def normalize(D: pd.core.frame.DataFrame)->pd.core.frame.DataFrame:
-    for col in D.columns:
-        if is_any_real_numeric_dtype(D[col]):
-            D[col] = normalize_column(D[col])
-    return D
-        
-
-
+def normalize_column(column):
+    """Normalizes a single column using min-max scaling."""
+    mins = np.min(column)
+    maxs = np.max(column)
+    return (column - mins) / (maxs - mins) if (maxs - mins) != 0 else column
