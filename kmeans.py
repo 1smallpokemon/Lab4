c9f5a3db4aa5b1f98b13bdc2447c71b4ef3bdaf5
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-06 16:17:18 -0800

    kmeans with random selection, and cluster compuation not working

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
+import numpy as np
 from preprocessing import *
+from DistanceCalculator import DistanceCalculator
 
 def main():
     # call preprocessing script, returns dataframe
@@ -21,8 +23,37 @@ def main():
     pass
 
 
-
-
+def initial_centroids(data , k):
+    return data.random_sample(n=k)
+    ## TODO Implement KMeans++ after :D
+    
+def kmeans(data, k):
+    centriods = initial_centroids(data, k)
+    
+    while stopping_condition(data, k):
+        # No clue why its called s
+        s = []
+        
+        # Assigns all points to no cluster, resets s to 0's
+        for i in range(k):
+            s.append(np.zeros(len(data[:1])).tolist())
+            clusters = [set() for _ in range(k)]
+        
+        for _,row in data.iterrows():
+            distances = np.zeros(k).tolist
+            for i in range(k):
+                distances[i] = DistanceCalculator.euclidian_distance(row, centriods[k])
+            
+            cluster = np.array(distances).argmin()
+            
+            clusters[cluster] | row
+            
+            s[i]
+            
+            
+        
+def stopping_condition(data, k):
+    return
 
 if __name__ == "__main__":
     main()
