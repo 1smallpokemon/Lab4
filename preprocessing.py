d95af060afa697b5cf5185fe7221cc8cb6d8e001
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-13 15:38:28 -0800

    fixed preprocessing?

--- preprocessing.py
+++ preprocessing.py
@@ -3,17 +3,11 @@ import numpy as np
 from pandas.api.types import is_any_real_numeric_dtype
 
 def load_data(file_path):
-    #Loads data from a CSV file, excluding columns marked with 0 in the first row.
-    with open(file_path, 'r') as file:
-        first_line = file.readline().strip().split(',')
-        # Determine the number of columns to expect based on the first line
-        expected_cols = sum([1 for val in first_line if val == '1'])
+    # Read the CSV file, skipping the first row
+    data = pd.read_csv(file_path, header=None)
+    use_columns = data.iloc[0, :].astype(bool).tolist()
     
-    # Read the CSV with the expected number of columns
-    data = pd.read_csv(file_path, header=None, usecols=range(expected_cols))
-    
-    # Get the binary vector from the first row and exclude columns accordingly
-    use_columns = data.iloc[0].astype(bool).tolist()
+    # Filter columns based on the binary inclusion vector
     data = data.loc[:, use_columns]
     
     # Remove the first row as it is just the binary vector
@@ -21,28 +15,28 @@ def load_data(file_path):
     return data.reset_index(drop=True)
 
 def preprocess_data(data):
+    # Ensure data is not empty
+    if data.empty:
+        raise ValueError("No data available to process.")
+
     # Check if the last column is a class label, indicated by a 0 in the first row.
-    has_class_label = not data.iloc[0, -1].astype(bool)
-    
-    # If there's a class label, separate it from the features.
+    has_class_label = not bool(data.iloc[0, -1])  # Using bool() directly for clarity
+
+    class_labels = None
     if has_class_label:
-        class_labels = data.iloc[1:, -1]  # Extract class labels from the data
+        class_labels = data.iloc[:, -1]  # Extract class labels from the data
         data = data.iloc[:, :-1]  # Remove class labels from the data
+
+    # Convert all columns to numeric, coerce errors, and drop rows with NaN values
+    data = data.apply(pd.to_numeric, errors='coerce').dropna()
+
+    # Normalize the data if it's not empty
+    if not data.empty:
+        data = normalize(data)
     else:
-        class_labels = None
-
-    # Convert all columns to numeric
-    for col in data.columns:
-        data[col] = pd.to_numeric(data[col], errors='coerce')
-
-    # Replace '?' with NaN and drop rows with NaN values
-    data = data.replace('?', np.nan).dropna()
-
-    # Normalize the data
-    data = normalize(data)
+        raise ValueError("All rows contain NaN after conversion to numeric.")
 
     return data, class_labels  # Return both the features and the class labels (if present)
-
 
 def normalize(data):
     # Normalizes the numeric columns in the data.
