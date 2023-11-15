09e24d856e3b47623bf799bf02c0901e53454118
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-14 17:24:31 -0800

    non working commit, but staging

--- tuning_kmeans.py
+++ tuning_kmeans.py
@@ -30,35 +30,45 @@ def main():
     # Initialize logging
     logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
     
+    # Initialize best parameters
     best_sse = np.inf
     best_k = None
     best_threshold = None
     best_purity = 0 if is_classification_dataset else None
-    
-     # Tuning k
+
+    # Tuning k
     for k in k_values:
         centroids, assignments = kmeans(data, k)
         sse = calculate_sse(data, centroids, assignments)
         logging.info(f"k={k}: SSE={sse}")
-        
+
+        # Update best parameters based on SSE and, if applicable, purity
+        update_params = False
         if is_classification_dataset:
             purity = calculate_purity(class_labels, assignments)
             logging.info(f"k={k}: Purity={purity}")
-            if purity > best_purity:
-                best_k = k
-                best_purity = purity     
+            # Update if purity is better, or if purity is equal and SSE is lower
+            if purity > best_purity or (purity == best_purity and sse < best_sse):
+                update_params = True
         elif sse < best_sse:
-            best_sse = best_sse
+            update_params = True
 
+        # Update best parameters
+        if update_params:
+            best_sse = sse
+            best_k = k
+            if is_classification_dataset:
+                best_purity = purity
 
     # Output best parameters for SSE
     print(f"Best SSE parameters: k={best_k} with SSE: {best_sse}")
     logging.info(f"Best SSE parameters: k={best_k} with SSE: {best_sse}")
 
     # Output best parameters for purity if applicable
-    if class_labels is not None:
+    if is_classification_dataset:
         print(f"Best Purity parameters: k={best_k} with Purity: {best_purity}")
         logging.info(f"Best Purity parameters: k={best_k} with Purity: {best_purity}")
+
 
 if __name__ == "__main__":
     main()
