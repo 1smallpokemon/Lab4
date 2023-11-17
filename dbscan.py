59df347452f85cfc5161e54de8645a3b42c7e4cc
Joseph Dewar <superjoeyd123@gmail.com>
2023-11-17 13:14:50 -0800

    clean

--- dbscan.py
+++ dbscan.py
@@ -4,46 +4,6 @@ import numpy as np
 from preprocessing import load_data, preprocess_data
 from DistanceCalculator import DistanceCalculator
 
-# def find_epsilon_neighborhood(point, data, epsilon):
-#     neighbors = []
-#     for idx, other_point in data.iterrows():
-#         if DistanceCalculator.euclidean_distance(point, other_point) < epsilon:
-#             neighbors.append(idx)
-#     return neighbors
-
-# def dbscan(data, epsilon, min_points):
-#     labels = [0]*data.shape[0]  # 0 indicates unvisited
-#     cluster_id = 0
-
-#     for idx, point in data.iterrows():
-#         if labels[idx] != 0:  # Skip if already visited
-#             continue
-
-#         # Find neighbors
-#         neighbors = find_epsilon_neighborhood(point, data, epsilon)
-
-#         if len(neighbors) < min_points:
-#             labels[idx] = -1  # Mark as noise
-#         else:
-#             cluster_id += 1
-#             labels[idx] = cluster_id
-#             grow_cluster(data, labels, cluster_id, neighbors, epsilon, min_points)
-
-#     return labels
-
-# def grow_cluster(data, labels, cluster_id, neighbors, epsilon, min_points):
-#     i = 0
-#     while i < len(neighbors):
-#         point_idx = neighbors[i]
-#         if labels[point_idx] == -1:
-#             labels[point_idx] = cluster_id  # Change noise to border point
-#         elif labels[point_idx] == 0:
-#             labels[point_idx] = cluster_id  # Add new point to cluster
-#             point_neighbors = find_epsilon_neighborhood(data.iloc[point_idx], data, epsilon)
-#             if len(point_neighbors) >= min_points:
-#                 neighbors += point_neighbors  # Add new neighbors to the list
-#         i += 1
-
 def euclidean_distance(point1, point2):
     return np.sqrt(np.sum((point1 - point2) ** 2))
 
